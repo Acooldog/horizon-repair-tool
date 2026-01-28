04a12c50ea6e8d50fe4552993b73e4e62180f241
daoges_x <2622138410@qq.com>
2026-01-29 06:19:04 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void click_test(object sender, EventArgs e)
+        {
+            Form2 dlog = new Form2();
+            dlog.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
